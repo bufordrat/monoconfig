8af11a0df581d222fc2c9e2919698abc9e88a2d5
Matt Teichman <teichman@uchicago.edu>
2025-02-05 12:39:49 -0600

    beginning to incorporate subtype

---
+++ fish/subtype.fish
@@ -0,0 +1,13 @@
+# start ssh-agent and gpg-agent upon login
+if status --is-login; and not set -q SSH_CLIENT
+   eval (ssh-agent -c)
+   ssh-add
+   gpg --decrypt --pinentry-mode loopback dummy.gpg > /dev/null
+   emacs --daemon
+end
+
+# opam stuff
+eval (opam env)
+
+# ghcup stuff
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/teichman/.ghcup/bin # ghcup-env
