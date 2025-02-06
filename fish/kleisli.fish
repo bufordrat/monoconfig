d2b4a6766bbf9c2a690f1cb8c63f98c4a76c0420
Matt Teichman <teichman@uchicago.edu>
2025-02-06 14:08:00 -0600

    add kleisli fish config

---
+++ fish/kleisli.fish
@@ -0,0 +1,8 @@
+# fire up ssh-agent and gpg-agent
+source ~/.config/fish/ssh_gpg.fish
+
+# opam stuff
+eval (opam env)
+
+# ghcup stuff
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/teichman/.ghcup/bin # ghcup-env
