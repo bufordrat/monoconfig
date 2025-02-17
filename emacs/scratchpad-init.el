1f7962e23c2c8ee0c2fa4f7b7a26d2f5a6a8c852
Matt Teichman <teichman@uchicago.edu>
2025-02-17 15:24:22 -0600

    messing around with GUI toggler

--- emacs/scratchpad-init.el
+++ emacs/scratchpad-init.el
@@ -1,8 +1,15 @@
-;; probably going to get rid of this
+(defun mt-turnon-current-direds (turnon)
+  (cl-loop
+   for buf being the buffers
+   do (with-current-buffer buf
+	(when (eq major-mode 'dired-mode)
+          (nerd-icons-dired-mode turnon)))))
 
-;; (defun mt-markdown-auto-export ()
-;;   (when (eq major-mode 'markdown-mode)
-;;     (eww-open-file (markdown-export))))
-
-;; (with-eval-after-load 'markdown
-;;   (add-to-list 'after-save-hook 'mt-markdown-auto-export))
+(defun mt-turnon-gui (turnon)
+  (if turnon
+    (progn (mt-turnon-current-direds turnon)
+	   (add-hook 'dired-mode-hook #'nerd-icons-dired-mode)
+	   (doom-modeline-mode 1))
+    (progn (mt-turnon-current-direds (not turnon))
+	     (remove-hook 'dired-mode-hook #'nerd-icons-dired-mode)
+	     (doom-modeline-mode 0))))
