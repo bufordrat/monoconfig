ed62794cb1ebb08263482540d1a05b5cfa28d2b8
Matt Teichman <teichman@uchicago.edu>
2025-02-17 19:09:05 -0600

    get rid of loading of general-init-nw, even for -nw hosts

--- emacs/entry-point.el
+++ emacs/entry-point.el
@@ -1,7 +1,5 @@
 ;; determine init code to load conditionally on hostname and os
 (setq host-init (concat (car (split-string (system-name) "\\.")) "-init"))
-;; (setq general-init (if (string-match-p "rpi" operating-system-release)
-;; 		       "general-init-nw" "general-init"))
 
 ;; load matt's personal elisp
 (with-demoted-errors "%s" (load-library "fonts"))
