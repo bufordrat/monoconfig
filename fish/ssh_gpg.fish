e3c84240f731a349014ac83f7f27a26c33e22aad
Matt Teichman <teichman@uchicago.edu>
2025-02-20 03:23:56 +0000

    ouch, it's --pinentry-mode loopback, not --pinentry=loopback

--- fish/ssh_gpg.fish
+++ fish/ssh_gpg.fish
@@ -2,7 +2,6 @@
 if status --is-login; and not set -q SSH_CLIENT
    eval (ssh-agent -c)
    ssh-add
-   gpg -d --pinentry=loopback dummy.gpg
-   # while not gpg -d --pinentry=loopback dummy.gpg 1> /dev/null 2>&1 ; : ; end
+   while not gpg -d --pinentry-mode loopback dummy.gpg 1> /dev/null 2>&1 ; : ; end
    emacs --daemon
 end
