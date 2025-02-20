51bff475c540c196e493cd49ef512135a6796736
Matt Teichman <teichman@uchicago.edu>
2025-02-19 20:58:08 -0600

    comment out gpg loop

--- fish/ssh_gpg.fish
+++ fish/ssh_gpg.fish
@@ -3,6 +3,6 @@ if status --is-login; and not set -q SSH_CLIENT
    eval (ssh-agent -c)
    ssh-add
    gpg -d --pinentry=loopback dummy.gpg
-   while not gpg -d --pinentry=loopback dummy.gpg 1> /dev/null 2>&1 ; : ; end
+   # while not gpg -d --pinentry=loopback dummy.gpg 1> /dev/null 2>&1 ; : ; end
    emacs --daemon
 end
