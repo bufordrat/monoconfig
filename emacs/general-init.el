81a006ec8fc9a1772447b64d6ea034bf343563ba
Matt Teichman <teichman@uchicago.edu>
2025-02-25 14:00:02 -0600

    new completion regime, yay

--- emacs/general-init.el
+++ emacs/general-init.el
@@ -76,11 +76,23 @@
   (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
 
 ;; completion
-(with-demoted-errors "%S"
-  (vertico-mode +1))
-(require 'orderless)
-(setq completion-styles '(orderless basic)
-      completion-category-overrides '((file (styles basic partial-completion))))
+(use-package vertico
+  :ensure t
+  :config (vertico-mode))
+(use-package marginalia
+  :ensure t
+  :custom
+  (marginalia-max-relative-age 0)
+  :init
+  (marginalia-mode))
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+(setq read-file-name-completion-ignore-case t)
+(setq read-buffer-completion-ignore-case t)
+(setq extended-command-suggest-shorter nil)
 
 ;; message mode
 (setq message-send-mail-function 'smtpmail-send-it)
