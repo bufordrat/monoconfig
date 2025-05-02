74ebbd4686c1f25bc473c170765ecf2e31d6c48c
Matt Teichman <teichman@uchicago.edu>
2025-05-02 09:28:25 -0500

    changed the window keybindings yet again to M-number

--- emacs/general-init.el
+++ emacs/general-init.el
@@ -17,14 +17,10 @@
 (global-set-key (kbd "C-c m") #'magit-clone)
 (global-set-key (kbd "M-+") #'scroll-up-line)
 (global-set-key (kbd "M-_") #'scroll-down-line)
-;; (global-set-key (kbd "C-*") #'split-window-right)
-(global-set-key (kbd "M-p") #'split-window-right)
-;; (global-set-key (kbd "C-&") #'split-window-below)
-(global-set-key (kbd "M-]") #'split-window-below)
-;; (global-set-key (kbd "C-^") #'delete-window)
-(global-set-key (kbd "M-i") #'delete-window)
-;; (global-set-key (kbd "C-+") #'delete-other-windows)
-(global-set-key (kbd "M-SPC") #'delete-other-windows)
+(global-set-key (kbd "M-3") #'split-window-right)
+(global-set-key (kbd "M-2") #'split-window-below)
+(global-set-key (kbd "M-0") #'delete-window)
+(global-set-key (kbd "M-1") #'delete-other-windows)
 
 ;; replacements for GUI Emacs keybindings
 (global-set-key (kbd "M-;") #'comment-line)
