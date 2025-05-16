85e97506187a11eb9ee77fc93fb78477ada8d403
Matt Teichman <teichman@uchicago.edu>
2025-05-16 14:54:50 -0500

    seems to work; probably needs a little more bug testing

--- emacs/ocaml.el
+++ emacs/ocaml.el
@@ -55,17 +55,6 @@
 		     old-path)))
     new-path))
 
-;; (defun ml-buffer-toggle ()
-;;   (let* ((old-path buffer-file-name)
-;; 	 (mli-path (derive-mli-path old-path))
-;; 	 (new-path (transform-path mli-path))
-;; 	 (mli-path-short (file-name-nondirectory mli-path))
-;; 	 (new-path-short (file-name-nondirectory new-path)))
-;;     (message (format
-;; 	      "renaming %s to %s"
-;; 	      mli-path-short
-;; 	      new-path-short))))
-
 (defun replace-extension (old-path new-extension)
   (let* ((old-filename (file-name-sans-extension old-path))
 	 (old-extension (file-name-extension old-path)))
@@ -99,22 +88,13 @@ delete one and try again."))
 	 (start-path-short (file-name-nondirectory start-path))
 	 (new-path-short (file-name-nondirectory new-path)))
     (validate-ml-path old-path)
-    (when (or (file-exists-p mli-path) (file-exists-p shutoffmli-path))
+    (when (or (file-exists-p mli-path)
+	      (file-exists-p shutoffmli-path))
       (rename-file start-path new-path)
-      (format "Renaming %s to %s..." start-path-short new-path-short))))
-
-;; - unless buffer-file-name is .ml, .mli, or .shutoffmli, or it's a dired buffer, error out
-;; - case 1: .ml
-;;   - calculate mli path by replacing buffer-file-name with mli
-;;   - calculate shutoffmli path by replacing buffer-file-name with shutoffmli
-;;   - if neither .mli nor .shutoffmli exist, error out
-;;   - if both .mli and .shutoffmli exist, error out
-;;   - if the .mli path exists, toggle it with rename-toggle code
-;;   - if the .shutoffmli path exists, toggle it with rename-toggle code
-;; - case 2: .mli or .shutoffmli
-;;   - delegate to mli-toggle code
-;; - case 3: dired
-;;   - delegate to dired toggle code
+      (message
+       (format "Renaming %s to %s..."
+	       start-path-short
+	       new-path-short)))))
 
 (defun mli-toggle ()
   (interactive)
@@ -122,14 +102,3 @@ delete one and try again."))
 	((suitable-mli-buffer-p) (mli-buffer-toggle))
 	((eq major-mode 'dired-mode) (mli-dired-toggle))
 	(t (error "You aren't in a Tuareg buffer."))))
-
-
-
-    ;; (cond (((and mli-exists shutoffmli-exists) (error "This .ml file has both .mli and .shutoffmli correspondents, which should be impossible.  Please synchronize and try again."))
-    ;; 	   ((and (not mli-exists) (not shutoffmli-exists)) (error "This .ml file has no .mli correspondent."))
-    ;; 	   (t nil)))))
-
-    ;; (cond (
-	   
-    ;; 	   (t nil)))
-
