a0b8ddecb7005e81c41ca1d904904078374b0fe9
Matt Teichman <teichman@uchicago.edu>
2025-05-16 14:45:14 -0500

    baby steps, mostly have ml logic hammered out; still need main logic

--- emacs/ocaml.el
+++ emacs/ocaml.el
@@ -70,16 +70,31 @@
 	 (shutoffmli-path (replace-extension path "shutoffmli"))
 	 (mli-exists (file-exists-p mli-path))
 	 (shutoffmli-exists (file-exists-p shutoffmli-path)))
-    (when (and mli-exists shutoffmli-exists) (error "This .ml file has both .mli and .shutoffmli correspondents, which should be impossible.  Please synchronize and try again."))
-    (when (and (not mli-exists) (not shutoffmli-exists)) (error "This .ml file has no .mli correspondent."))))
+    (when (and mli-exists shutoffmli-exists)
+      (error
+       ".mli and .shutoffmli correspondents of this file both exist; please
+delete one and try again."))
+    (when (and (not mli-exists)
+	       (not shutoffmli-exists))
+      (error "This .ml file has no .mli correspondent."))))
 
-    ;; (cond (((and mli-exists shutoffmli-exists) (error "This .ml file has both .mli and .shutoffmli correspondents, which should be impossible.  Please synchronize and try again."))
-    ;; 	   ((and (not mli-exists) (not shutoffmli-exists)) (error "This .ml file has no .mli correspondent."))
-    ;; 	   (t nil)))))
+(defun the-file-at (path1 path2)
+  (if (file-exists-p path1) path1 path2))
 
-    ;; (cond (
-	   
-    ;; 	   (t nil)))
+(defun ml-buffer-toggle ()
+  (let* ((old-path buffer-file-name)
+	 (mli-path (replace-extension old-path "mli"))
+	 (shutoffmli-path (replace-extension old-path "shutoffmli"))
+	 (mli-exists (file-exists-p mli-path))
+	 (shutoffmli-exists (file-exists-p shutoffmli-path))
+	 (start-path (the-file-at mli-path shutoffmli-path))
+	 (new-path (transform-path start-path))
+	 (start-path-short (file-name-nondirectory start-path))
+	 (new-path-short (file-name-nondirectory new-path)))
+    (validate-ml-path old-path)
+    (when (or (file-exists-p mli-path) (file-exists-p shutoffmli-path))
+      (rename-file start-path new-path)
+      (format "Renaming %s to %s..." start-path-short new-path-short))))
 
 ;; - unless buffer-file-name is .ml, .mli, or .shutoffmli, or it's a dired buffer, error out
 ;; - case 1: .ml
@@ -99,3 +114,14 @@
   (cond ((suitable-mli-buffer-p) (mli-buffer-toggle))
 	((eq major-mode 'dired-mode) (mli-dired-toggle))
 	(t (error "You aren't in a Tuareg buffer."))))
+
+
+
+    ;; (cond (((and mli-exists shutoffmli-exists) (error "This .ml file has both .mli and .shutoffmli correspondents, which should be impossible.  Please synchronize and try again."))
+    ;; 	   ((and (not mli-exists) (not shutoffmli-exists)) (error "This .ml file has no .mli correspondent."))
+    ;; 	   (t nil)))))
+
+    ;; (cond (
+	   
+    ;; 	   (t nil)))
+
