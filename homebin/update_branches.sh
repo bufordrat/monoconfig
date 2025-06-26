cf1045eea54368663d924de6d321a28775adec7c
Matt Teichman <teichman@uchicago.edu>
2025-06-26 09:06:10 -0500

    have update_branches only do a git fetch --all

--- homebin/update_branches.sh
+++ homebin/update_branches.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-NOT=
-find $1 -maxdepth 1 -type d ! -name .\* |
+find $1 -maxdepth 1 -mindepth 1 -type d ! -name .\* |
 while read dir
 do
-  (
-    cd "$dir"
-    $NOT git fetch --all
-    for branch in $(git branch -a --format "%(refname:short)" | sed '/^origin\//!d;s;^origin/;;');
-    do $NOT git checkout    "$branch";
-       $NOT git pull origin "$branch" ;
-    done
-  )
+    (
+	echo git -C $dir fetch --all
+	git -C $dir fetch --all
+    )
 done
