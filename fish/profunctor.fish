1f83b8f2ed847a7891613ee25d1d566666a50d22
Matt Teichman <teichman@uchicago.edu>
2025-08-19 15:03:13 -0500

    some profunctor updates

--- fish/profunctor.fish
+++ fish/profunctor.fish
@@ -1,3 +1,12 @@
 # add homebrew to path
+fish_add_path /opt/homebrew/opt/make/libexec/gnubin
 fish_add_path /opt/homebrew/bin
 
+# add ~/local/bin to path for Agda
+fish_add_path ~/.local/bin
+
+# opam stuff
+eval (opam env)
+
+# ghcup stuff
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/teichman/.ghcup/bin $PATH # ghcup-env
