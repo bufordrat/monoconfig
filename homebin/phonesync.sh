13c21bdf51072282a297bb90bcc7f09fbae9fa9b
Matt Teichman <teichman@uchicago.edu>
2025-12-08 13:04:06 -0600

    got photo sync on kleisli happening with pixel 10

--- homebin/phonesync.sh
+++ homebin/phonesync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 LOCAL_PATH=~/Stuff/bkup/phone/current
-REMOTE_PATH='pixel8:~/storage/shared/Phone/'
+REMOTE_PATH='pixel10:~/storage/shared/Phone/'
 rsync -aizvP -e 'ssh -p 8022' $REMOTE_PATH $LOCAL_PATH
