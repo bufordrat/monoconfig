1d89b133fe8937b6c97d05a2722d36242212d2f4
Matt Teichman <teichman@uchicago.edu>
2025-12-12 10:28:27 -0600

    change pulsarsync back not to be quiet

--- homebin/pulsarsync.sh
+++ homebin/pulsarsync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 LOCAL_PATH=~/.android-music/
 REMOTE_PATH='pixel10:~/storage/shared/Music/android-music'
-rsync -aizvP --out-format='android music update: %i %n' --delete --no-perms $LOCAL_PATH $REMOTE_PATH | grep -oE '^android music update:.*' || true
+rsync -aizvP --delete --no-perms $LOCAL_PATH $REMOTE_PATH
