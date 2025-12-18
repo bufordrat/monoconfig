7d18cf916f400a623ec6bc3a08bdf225d2c1c412
Matt Teichman <teichman@uchicago.edu>
2025-12-18 11:46:33 -0600

    jump into the trivy json and pull vulnerabilities out

--- homebin/trivies.sh
+++ homebin/trivies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for project in $@
 do
-    trivy fs --quiet --scanners vuln,secret --format json ~/stuff/github/$project | jq '.Results | select (. != null)'
+    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[].Vulnerabilities | select (. != null)'
 done
