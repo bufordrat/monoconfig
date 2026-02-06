37342d524282b3e4727831acaee90c1afd6ceff4
Matt Teichman <teichman@uchicago.edu>
2026-02-06 11:24:27 -0600

    update to trivies

--- homebin/trivies.sh
+++ homebin/trivies.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 for project in $@
 do
-    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)' 
+    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityId, "References": .References }'
+    # trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)' 
 done
+
+
