b9bbda5b42693404dbb822ad76ad6f601ace508b
Matt Teichman <teichman@uchicago.edu>
2026-02-06 11:29:45 -0600

    fix VulnerabilityID bug in trivies

--- homebin/trivies.sh
+++ homebin/trivies.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 for project in $@
 do
-    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityId, "References": .References }'
-    # trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)' 
+    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityID, "References": .References }'
 done
 
 
