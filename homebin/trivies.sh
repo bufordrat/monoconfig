9bf0b464f8ae3e0145359524600a5f446571d2b3
Matt Teichman <teichman@uchicago.edu>
2026-03-12 11:48:39 -0500

    update sequent crontab

--- homebin/trivies.sh
+++ homebin/trivies.sh
@@ -4,5 +4,3 @@ for project in $@
 do
     trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityID, "References": .References }'
 done
-
-
