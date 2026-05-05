6888a13ec626c932f5aaa3b934780943fb549556
Matt Teichman <teichman@uchicago.edu>
2026-05-05 17:25:55 -0500

    add mercurial to arch packages in makefile

--- homebin/sexp_susp.sh
+++ homebin/sexp_susp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(ssh sequent "emacsclient -eval '(zap-waypiped-client-frames)'"; systemctl suspend; sleep 0.1) && waylock
+((nmcli -t -f name,active connection show | grep "UChicagoVPN:yes" || nmcli -t -f name,active connection show | grep "eduroam:yes" || nmcli -t -f name,active connection show | grep "CVPN:yes") && ssh sequent "emacsclient -eval '(zap-waypiped-client-frames)'"; systemctl suspend; sleep 0.1) && waylock
