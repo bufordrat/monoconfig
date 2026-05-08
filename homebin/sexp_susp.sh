52190c38ccf09131fc639d2f43b57c4f115bcf05
Matt Teichman <teichman@uchicago.edu>
2026-05-08 15:40:13 -0500

    put suspend command back since my stuff isn't going to fix the GTK bug

--- homebin/sexp_susp.sh
+++ homebin/sexp_susp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-((nmcli -t -f name,active connection show | grep "UChicagoVPN:yes" || nmcli -t -f name,active connection show | grep "eduroam:yes" || nmcli -t -f name,active connection show | grep "CVPN:yes") && ssh sequent "emacsclient -eval '(zap-waypiped-client-frames)'"; systemctl suspend; sleep 0.1) && waylock
+(systemctl suspend; sleep 0.1) && waylock
