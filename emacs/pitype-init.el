9b93773d61598bdbf711a0d1828b3d9ff52b5cff
Matt Teichman <teichman@uchicago.edu>
2026-05-25 10:12:18 -0500

    remove inconsolata stuff from pitype emacs init file

--- emacs/pitype-init.el
+++ emacs/pitype-init.el
@@ -1,7 +1,3 @@
-(defconst matt-default-font "Inconsolata-12")
-(add-to-list 'default-frame-alist (cons 'font matt-default-font))
-(set-face-font 'default matt-default-font)
-
 ;; setopts
 (setq org-file-apps
    '((auto-mode . emacs)
