d5ab0f259486b85f72d63696d01e5ad30c30c0ee
Matt Teichman <teichman@uchicago.edu>
2026-08-04 08:41:21 -0500

    fix trivy setup so that the whole argv of trivies.sh gets piped into jq then cvebump

--- homebin/trivies.sh
+++ homebin/trivies.sh
@@ -2,5 +2,5 @@
 trivy clean --all 2> /dev/null
 for project in $@
 do
-    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityID, "References": .References }' | jq -n '[inputs]'
+    trivy fs --quiet --scanners vuln --format json ~/stuff/github/$project | jq '.Results[]?.Vulnerabilities | select (. != null)[] | {"PkgName" : .PkgName, "InstalledVersion" : .InstalledVersion, "FixedVersion": .FixedVersion, "Severity" : .Severity, "VulnerabilityID" : .VulnerabilityID, "References": .References }'
 done 2> /dev/null
