58518cdcb0502d87dd1b760ff7231a882c68f7a9
Matt Teichman <teichman@uchicago.edu>
2026-08-06 15:38:52 -0500

    fix manpages and ssh-agent in fish config

--- fish/general.fish
+++ fish/general.fish
@@ -69,5 +69,3 @@ set -x LESS_TERMCAP_us (printf "\033[01;32m")
 # global variables
 set -gx DIFFPROG diffmerge
 set -gx EDITOR figure_out_editor_variable
-set -gx MANPATH /usr/share/man
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
--- fish/sexp.fish
+++ fish/sexp.fish
@@ -9,3 +9,9 @@ fish_add_path ~/.cabal/bin
 
 # more local binary paths
 fish_add_path ~/.local/bin
+
+# make fish aware of ssh agent
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+# linux man pages
+set -gx MANPATH /usr/share/man
