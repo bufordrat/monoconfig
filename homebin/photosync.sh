6f3ccb2ffbac8686350ad8595d64b79098d573de
Matt Teichman <teichman@uchicago.edu>
2026-09-09 06:34:14 -0500

    update photosync to the new adb regime

--- homebin/photosync.sh
+++ homebin/photosync.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-LOCAL_PATH=~/Stuff/Photos/pixel10/current
-REMOTE_PATH='pixel10:~/storage/shared/DCIM/Camera/'
-rsync -aizvP --out-format='photos update: %i %n' --delete $REMOTE_PATH $LOCAL_PATH | grep -oE '^photos update:.*' || true
+ANDROID_PATH=/sdcard/DCIM/Camera
+ADB_DUMP_PATH=~/adb-dump
+RSYNC_TARGET=~/Stuff/Photos/pixel10/current
+RSYNC_SOURCE=~/adb-dump/Camera/
+adb pull -a $ANDROID_PATH $ADB_DUMP_PATH && rsync -aizvP --out-format='photos update: %i %n' --delete $RSYNC_SOURCE $RSYNC_TARGET | grep -oE '^photos update:.*' || true
