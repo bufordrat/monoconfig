94bda66c05bac14ee4484d16afdafad1d3694253
Matt Teichman <teichman@uchicago.edu>
2026-09-16 20:40:38 -0500

    update phonesync and photosync

--- homebin/photosync.sh
+++ homebin/photosync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ANDROID_PATH=/sdcard/DCIM/Camera
 ADB_DUMP_PATH=~/adb-dump
-RSYNC_TARGET=~/Stuff/Photos/pixel10/current
 RSYNC_SOURCE=~/adb-dump/Camera/
-adb pull -a $ANDROID_PATH $ADB_DUMP_PATH && rsync -aizvP --out-format='photos update: %i %n' --delete $RSYNC_SOURCE $RSYNC_TARGET | grep -oE '^photos update:.*' || true
+RSYNC_TARGET=~/Stuff/Photos/pixel10/current
+adb pull -a $ANDROID_PATH $ADB_DUMP_PATH 2> /dev/null && rsync -aizvP --out-format='photos update: %i %n' --delete $RSYNC_SOURCE $RSYNC_TARGET | grep -oE '^photos update:.*' || true
