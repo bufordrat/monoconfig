daa23e6c3681718689614049886775fb80399405
Matt Teichman <teichman@uchicago.edu>
2026-09-19 09:53:59 -0500

    remove commented out keybindings and agent shell use-package

--- emacs/general-init.el
+++ emacs/general-init.el
@@ -11,47 +11,6 @@
 ;; cd to homedir
 (cd "~")
 
-;; ;; keybindings
-;; (global-set-key (kbd "C-c v") #'visual-line-mode)
-;; (global-set-key (kbd "C-c f p") #'mt-change-font-family)
-;; (global-set-key (kbd "C-c f h") #'mt-change-font-size)
-;; (global-set-key (kbd "C-c f n") #'mt-hires-sequent-client)
-;; (global-set-key (kbd "C-c g") #'gui-mode)
-;; (global-set-key (kbd "C-c \\") #'mt-shell)
-;; (global-set-key (kbd "C-c |") #'eshell)
-;; (global-set-key (kbd "C-c m") #'magit-clone)
-;; (global-set-key (kbd "C-c i") #'mli-toggle)
-;; (global-set-key (kbd "M-+") #'scroll-up-line)
-;; (global-set-key (kbd "M-_") #'scroll-down-line)
-;; (global-set-key (kbd "M-3") #'split-window-right)
-;; (global-set-key (kbd "M-2") #'split-window-below)
-;; (global-set-key (kbd "M-0") #'delete-window)
-;; (global-set-key (kbd "M-1") #'delete-other-windows)
-
-;; ;; replacements for GUI Emacs keybindings
-;; (global-set-key (kbd "M-;") #'comment-line)
-;; (global-set-key (kbd "M-o") #'other-window)
-;; (global-set-key (kbd "C-x ,") #'previous-buffer)
-;; (global-set-key (kbd "C-x .") #'next-buffer)
-;; (global-set-key (kbd "C-c 9") #'winner-undo)
-;; (global-set-key (kbd "C-c 0") #'winner-redo)
-
-;; ;; function calls
-;; (winner-mode 1)
-;; (windmove-default-keybindings)
-;; (size-indication-mode 1)
-;; (display-time)
-;; (show-paren-mode 1)
-
-;; ;; menu bar stuff
-;; (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-;; (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-;; (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
-
-;; ;; column numbers
-;; (column-number-mode 1)
-
 ;; package-initialize
 (setq package-enable-at-startup nil)
 (package-initialize)
@@ -75,8 +34,6 @@
 (setq default-major-mode 'fundamental-mode)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 (setq ediff-split-window-function 'split-window-horizontally)
-
-;; TODO: fix ansi escape nonsense in man mode
 
 ;; fix ansi escape nonsense while compiling
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
@@ -307,11 +264,6 @@
 
 ;; htmlize
 (use-package htmlize
-  :ensure t
-  :defer t)
-
-;; agent-shell
-(use-package agent-shell
   :ensure t
   :defer t)
 
